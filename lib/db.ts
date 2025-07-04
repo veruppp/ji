45a623123d323d53a3cea88f2e0cdbbdce769e2d
veruppp <pp4412542@gmail.com>
2025-07-04 13:59:24 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,5 @@
+// lib/db.ts
+import { PrismaClient } from '@prisma/client';
 
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const prisma = new PrismaClient();
+export default prisma;
